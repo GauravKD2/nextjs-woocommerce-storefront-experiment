952287d72eef24674b6ff14950ef4686ecaff4bb
Gaurav KD <GauravKD@users.noreply.github.com>
2025-11-27 16:55:25 +0530

    Export 'Out' fix

--- next.config.js
+++ next.config.js
@@ -3,18 +3,16 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  output: 'export',  // Add this for static export
   experimental: {
     nextScriptWorkers: true,
   },
-  // Remove or comment out redirects() - incompatible with static export [web:33]
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/home',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
 })
